a0758041892808205be65ec85eaa64fedcc603c5
Pmookerjee <pmookerjee@gmail.com>
2017-04-15 13:00:35 -0700

    Completed the todos

--- starter-code/server.js
+++ starter-code/server.js
@@ -9,16 +9,19 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+
+
 // TODO: Include all of the static resources as an argument to app.use()
 app.use(express.static(__dirname + '/public'));
-
+//
 app.get('*', function(request, response) {
   // TODO: Using the response object, send the index.html file back to the user
-  response.sendFile(__dirname + '/index.html');
+  response.sendFile(__dirname + './index.html');
 });
 
 // TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
 
 app.listen(PORT, function() {
   // TODO: Log to the console a message that lets you know which port your server has started on
+  console.log('Server is running on port: ' + PORT);
 });
